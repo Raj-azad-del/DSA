d4cc0c3b6021e8661d74fe53a55ca250904b7ddb
Raj kamal Azad <rajazad261@gmail.com>
2026-09-03 16:02:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (6.53%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int k=0;
+        if(s.size()==0)return true;
+        if(t.size()==0 || t.size()<s.size())return false;
+       for(int i=0;i<t.size();i++){
+        if(s[k]==t[i] && i<t.size()){
+            if(k==s.size()-1)return true;
+            k++;
+        }
+        if((k!=s.size()-1 && i==t.size()-1)||(k==s.size()-1 && i==t.size()-1 && s[k]!=t[i])){
+            return false;
+        }
+       }
+       return true; 
+    }
+};
