9156b8a26a91a87cae86512c7565ec75e419b187
Raj kamal Azad <rajazad261@gmail.com>
2026-09-03 15:48:20 +0530

    Add Easy C++ Solution for 392. Is Subsequence from LeetCode

--- LeetCode/C++/Easy/392. Is Subsequence/Solution.cpp
+++ LeetCode/C++/Easy/392. Is Subsequence/Solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         int k=0;
-        if
+        if(s.size()==0)return true;
        for(int i=0;i<t.size();i++){
         if(s[k]==t[i] && i<t.size()){
             k++;continue;
