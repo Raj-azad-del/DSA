ed0421bf568189f8ea91d30ff6afd593d0d6e765
Raj kamal Azad <rajazad261@gmail.com>
2026-09-08 05:32:31 +0530

    Add Medium C++ Solution for 198. House Robber from LeetCode

--- LeetCode/C++/Medium/198. House Robber/Solution.cpp
+++ LeetCode/C++/Medium/198. House Robber/Solution.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int sum=0;
-        int new_sum=0;
-     for(int i=0;i<nums.size();i+=2){
-        sum=sum+nums[i];
-     }   
-     for(int i=1;i<nums.size();i+=2){
-        new_sum=new_sum+nums[i];
-     }
-     int res=max(sum,new_sum);
-     return res;
+        int a = 0;
+        int b = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            int c = max(b, a + nums[i]);
+            a = b;
+            b = c;
+        }
+        return b;
     }
 };
