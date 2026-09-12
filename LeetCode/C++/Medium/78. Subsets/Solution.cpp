f5cc7cc0e043daae2fc887cbb535e81de8288403
Raj kamal Azad <rajazad261@gmail.com>
2026-09-13 01:20:07 +0530

    Add Medium C++ Solution for 78. Subsets from LeetCode

--- LeetCode/C++/Medium/78. Subsets/Solution.cpp
+++ LeetCode/C++/Medium/78. Subsets/Solution.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>k;
-        int beg=0;
-        int end=nums.size();
-        vector<int>v;
-        k.push_back(v);
-        for(int i=0;i<nums.size();i++){
-            int s=k.size();
-            for(int j=0;j<s;j++){
-                vector<int>z=k[j];
-                z.push_back(nums[i]);
-                k.push_back(z);
-            }
+    void solve(vector<int>nums,vector<vector<int>>&v,int index,vector<int>vi){
+        if(index>=nums.size()){
+            v.push_back(vi);
+            return;
         }
-        return k;
+        vi.push_back(nums[index]);
+        solve(nums,v,index+1,vi);
+        vi.pop_back();
+        solve(nums,v,index+1,vi);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+     vector<vector<int>>v;
+     vector<int>vi;
+     solve(nums,v,0,vi);
+     return v;  
     }
 };
