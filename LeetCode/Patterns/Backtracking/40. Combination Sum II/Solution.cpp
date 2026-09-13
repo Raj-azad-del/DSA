8c15dc190d33d6125c94dca4beeaee95dd8060e6
Raj kamal Azad <rajazad261@gmail.com>
2026-09-13 07:47:04 +0530

    Add Medium C++ Solution for 40. Combination Sum II from LeetCode

--- LeetCode/Patterns/Backtracking/40. Combination Sum II/Solution.cpp
+++ LeetCode/Patterns/Backtracking/40. Combination Sum II/Solution.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    void usingrecursion(int index,int target,vector<int>& candidates,vector<int>& temp,vector<vector<int>>& v){
-        if(index==candidates.size()){
-            if(target==0)v.push_back(temp);
+    void solve(vector<vector<int>>&v,vector<int>vi,vector<int>nums,int target,int sum,int index){
+        if(sum==target){
+            sort(vi.begin(),vi.end());
+            if(find(v.begin(),v.end(),vi)==v.end()){
+            v.push_back(vi);
+            return;
+            }
+            else{
+                return;
+            }
+        }
+        if(index==nums.size()){
             return;
         }
-        if(candidates[index]<=target){
-            temp.push_back(candidates[index]);
-            usingrecursion(index+1,target-candidates[index],candidates,temp,v);
-            temp.pop_back();
-        }
-        while(index+1<candidates.size() && candidates[index]==candidates[index+1])
-            index++;
-        usingrecursion(index+1,target,candidates,temp,v);
+        vi.push_back(nums[index]);
+        sum=sum+nums[index];
+        solve(v,vi,nums,target,sum,index+1);
+        sum=sum-nums[index];
+        vi.pop_back();
+        solve(v,vi,nums,target,sum,index+1);
+
     }
-    vector<vector<int>> combinationSum2(vector<int>& candidates,int target){
-        vector<int>temp;
-        sort(candidates.begin(),candidates.end());
-        vector<vector<int>>v;
-        usingrecursion(0,target,candidates,temp,v);
-        return v;
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+      vector<vector<int>>v;
+      vector<int>vi;
+      solve(v,vi,candidates,target,0,0);
+      sort(v.begin(),v.end());
+      return v;  
     }
 };
